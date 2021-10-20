fc5b2d6323c9c00cce9b41cc65e5414c3e0db966
Peter John Saliente <psaliente@allegromicro.com>
2021-10-20 15:24:32 +0800

    set static on express

--- movie-house-api.js
+++ movie-house-api.js
@@ -7,6 +7,7 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 app.use(cors());
+app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
